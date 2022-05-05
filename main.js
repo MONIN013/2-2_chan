88d8daf54864b953d876465fba4c05902468fcf6
MONIN013 <103298017+MONIN013@users.noreply.github.com>
2022-05-05 16:55:33 +0900

    init

---
+++ main.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const fs = require("fs");
+const ejs = require("ejs");
+const { response } = require("express");
+
+const app = express();
+//app.use(express.static("static"));
+app.use(express.urlencoded({ extended: true }));
+
+let txtLogs = []; 
+let txtLogsApp = [];
+app.get("/", (request, response) => {
+    const template = fs.readFileSync("template.ejs","utf-8");
+    const html = ejs.render(template, {txtLogs: txtLogs});
+    response.send(html);
+});
+
+app.post("/send", (request, response) => {
+    
+    txtLogs.push(request.body.q);
+    txtLogsApp.push(`${txtLogs.length}. ${request.body.q}____${new Date().toString()}`);
+    /*
+    const template = fs.readFileSync("template.ejs","utf-8");
+    const html = ejs.render(template, {txtLogs: txtLogs});
+    response.send(html);
+    */
+    response.redirect("/");
+});
+app.listen(3000);
